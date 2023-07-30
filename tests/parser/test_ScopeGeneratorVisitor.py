8d6d2ecd0eb070d0100917b570d55457a0256753
uuk0 <baulukas1301@googlemail.com>
2023-07-30 19:18:47 +0200

    added unit test for name validation

--- tests/parser/test_ScopeGeneratorVisitor.py
+++ tests/parser/test_ScopeGeneratorVisitor.py
@@ -9,9 +9,50 @@ class TestScopeGeneratorVisitor(TestCase):
         scope = Parser.Scope()
         visitor = TypeResolver.ScopeGeneratorVisitor(scope)
 
-# sourcery skip: no-loop-in-tests
+        # sourcery skip: no-loop-in-tests
         for item in expr:
             visitor.visit_any(item)
 
         self.assertEqual({"test"}, scope.variable_name_stack)
+
+    def test_name_validation_valid(self):
+        parser = Parser.Parser("test = 10\na = test")
+        expr = parser.parse()
+
+        scope = Parser.Scope()
+        visitor = TypeResolver.ScopeGeneratorVisitor(scope)
+
+        # sourcery skip: no-loop-in-tests
+        for item in expr:
+            visitor.visit_any(item)
+
+        self.assertEqual({"test", "a"}, scope.variable_name_stack)
+
+        visitor = TypeResolver.LocalNameValidator()
+        # sourcery skip: no-loop-in-tests
+        for item in expr:
+            visitor.visit_any(item)
+
+    def test_name_validation_invalid(self):
+        parser = Parser.Parser("test = 10\na = test2")
+        expr = parser.parse()
+
+        scope = Parser.Scope()
+        visitor = TypeResolver.ScopeGeneratorVisitor(scope)
+
+        # sourcery skip: no-loop-in-tests
+        for item in expr:
+            visitor.visit_any(item)
+
+        self.assertEqual({"test", "a"}, scope.variable_name_stack)
+
+        visitor = TypeResolver.LocalNameValidator()
+        try:
+            # sourcery skip: no-loop-in-tests
+            for item in expr:
+                visitor.visit_any(item)
+        except NameError:
+            pass
+        else:
+            self.fail()
 
