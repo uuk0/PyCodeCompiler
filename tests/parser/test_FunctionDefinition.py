c773a5519bcd70cfa1fc56d56d6b0a302ae4a53f
uuk0 <baulukas1301@googlemail.com>
2023-07-31 15:36:26 +0200

    added generic tests

--- tests/parser/test_FunctionDefinition.py
+++ tests/parser/test_FunctionDefinition.py
@@ -24,3 +24,68 @@ class TestFunctionDefinition(TestCase):
             ],
         )], expr)
 
+    def test_generic_simple(self):
+        parser = Parser.Parser("def xy[a](): \n    x = 10")
+        expr = parser.parse()
+        self.assertEqual([FunctionDefinitionNode(
+            TokenType.IDENTIFIER("xy"),
+            [
+                TokenType.IDENTIFIER("a")
+            ],
+            [],
+            [
+                PyNewlineNode(TokenType.NEWLINE("\n")),
+                AssignmentExpression(
+                    [
+                        NameAccessExpression(TokenType.IDENTIFIER("x"))
+                    ],
+                    TokenType.EQUAL_SIGN("="),
+                    ConstantAccessExpression(10),
+                ),
+            ],
+        )], expr)
+
+    def test_generic_double(self):
+        parser = Parser.Parser("def xy[a, b](): \n    x = 10")
+        expr = parser.parse()
+        self.assertEqual([FunctionDefinitionNode(
+            TokenType.IDENTIFIER("xy"),
+            [
+                TokenType.IDENTIFIER("a"),
+                TokenType.IDENTIFIER("b"),
+            ],
+            [],
+            [
+                PyNewlineNode(TokenType.NEWLINE("\n")),
+                AssignmentExpression(
+                    [
+                        NameAccessExpression(TokenType.IDENTIFIER("x"))
+                    ],
+                    TokenType.EQUAL_SIGN("="),
+                    ConstantAccessExpression(10),
+                ),
+            ],
+        )], expr)
+
+    def test_generic_double_trailing(self):
+        parser = Parser.Parser("def xy[a, b,](): \n    x = 10")
+        expr = parser.parse()
+        self.assertEqual([FunctionDefinitionNode(
+            TokenType.IDENTIFIER("xy"),
+            [
+                TokenType.IDENTIFIER("a"),
+                TokenType.IDENTIFIER("b"),
+            ],
+            [],
+            [
+                PyNewlineNode(TokenType.NEWLINE("\n")),
+                AssignmentExpression(
+                    [
+                        NameAccessExpression(TokenType.IDENTIFIER("x"))
+                    ],
+                    TokenType.EQUAL_SIGN("="),
+                    ConstantAccessExpression(10),
+                ),
+            ],
+        )], expr)
+
