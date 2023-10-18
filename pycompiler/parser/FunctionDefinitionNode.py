5953b73f0e8c1c2c317dcf2a595bd7d659fc9359
uuk0 <baulukas1301@googlemail.com>
2023-10-18 21:56:24 +0200

    small refactor to FunctionDefinitionNode.py

--- pycompiler/parser/FunctionDefinitionNode.py
+++ pycompiler/parser/FunctionDefinitionNode.py
@@ -148,58 +148,9 @@ class FunctionDefinitionNode(AbstractSyntaxTreeNode):
         generic_pair = None, None
 
         if opening.token_type == TokenType.OPENING_SQUARE_BRACKET:
-            parser.push_state()
-            generic_name = parser.lexer.parse_token()
-
-            if generic_name.token_type == TokenType.CLOSING_SQUARE_BRACKET:
-                generic_pair = opening, generic_name
-                opening = None
-                parser.pop_state()
-
-            elif generic_name.token_type != TokenType.IDENTIFIER:
-                parser.rollback_state()
-                parser.lexer.raise_positioned_syntax_error(
-                    "expected <name> after '[' in <function definition> - <generics>"
-                )
-
-            else:
-                parser.pop_state()
-                generics.append(generic_name)
-
-                while True:
-                    parser.push_state()
-                    comma = parser.lexer.parse_token()
-
-                    if comma.token_type == TokenType.CLOSING_SQUARE_BRACKET:
-                        generic_pair = opening, comma
-                        opening = None
-                        parser.pop_state()
-                        parser.pop_state()
-                        break
-
-                    elif comma.token_type != TokenType.COMMA:
-                        parser.rollback_state()
-                        parser.lexer.raise_positioned_syntax_error(
-                            "expected ',' or ']' after <name> in <function definition> - <generics>"
-                        )
-
-                    generic_commas.append(comma)
-
-                    generic_name = parser.lexer.parse_token()
-                    if generic_name.token_type == TokenType.CLOSING_SQUARE_BRACKET:
-                        generic_pair = opening, generic_name
-                        opening = None
-                        parser.pop_state()
-                        parser.pop_state()
-                        break
-
-                    elif generic_name.token_type != TokenType.IDENTIFIER:
-                        parser.lexer.raise_positioned_syntax_error(
-                            "expected <name> or ']' after ','  in <function definition> - <generics>"
-                        )
-
-                    parser.pop_state()
-                    generics.append(generic_name)
+            generic_pair, opening = cls.parse_generics(
+                generic_commas, generic_pair, generics, opening, parser
+            )
 
         if opening is None:
             parser.pop_state()
@@ -214,124 +165,7 @@ class FunctionDefinitionNode(AbstractSyntaxTreeNode):
 
         parser.pop_state()
 
-        parameters = []
-
-        while True:
-            parser.push_state()
-            token = parser.lexer.parse_token()
-
-            if token.token_type == TokenType.CLOSING_ROUND_BRACKET:
-                parser.pop_state()
-                break
-
-            if token.token_type == TokenType.STAR:
-                parser.push_state()
-                next_token = parser.lexer.parse_token()
-
-                if next_token.token_type == TokenType.STAR:
-                    parser.pop_state()
-                    parser.push_state()
-                    param_name = parser.lexer.parse_token()
-
-                    if param_name.token_type != TokenType.IDENTIFIER:
-                        parser.rollback_state()
-                        parser.lexer.raise_positioned_syntax_error(
-                            "expected <expression> or '*' after '*'"
-                        )
-
-                    parser.pop_state()
-                    parser.pop_state()
-
-                    parameters.append(
-                        FunctionDefinitionArg(
-                            ArgType.STAR_STAR,
-                            param_name.text,
-                            name_token=token,
-                        )
-                    )
-
-                else:
-                    parser.rollback_state()
-                    parser.push_state()
-                    param_name = parser.lexer.parse_token()
-
-                    if param_name.token_type != TokenType.IDENTIFIER:
-                        parser.rollback_state()
-                        parser.lexer.raise_positioned_syntax_error(
-                            "expected <expression> or '*' after '*'"
-                        )
-
-                    parser.pop_state()
-                    parser.pop_state()
-
-                    parameters.append(
-                        FunctionDefinitionArg(
-                            ArgType.STAR,
-                            param_name.text,
-                            name_token=token,
-                        )
-                    )
-
-            else:
-                is_keyword = False
-                if token.token_type == TokenType.IDENTIFIER:
-                    next_token = parser.lexer.parse_token()
-
-                    if next_token.token_type == TokenType.EQUAL_SIGN:
-                        is_keyword = True
-
-                        expr = parser.try_parse_expression()
-
-                        if expr is None:
-                            parser.lexer.raise_positioned_syntax_error(
-                                "expected <expression> after '='"
-                            )
-
-                        parser.pop_state()
-                        parameters.append(
-                            FunctionDefinitionArg(
-                                ArgType.KEYWORD,
-                                token.text,
-                                expr,
-                                name_token=token,
-                                equal_sign_token=next_token,
-                            )
-                        )
-                    else:
-                        parser.rollback_state()
-
-                else:
-                    parser.rollback_state()
-
-                if not is_keyword:
-                    expr = parser.try_parse_expression()
-
-                    if expr is None:
-                        parser.lexer.raise_positioned_syntax_error(
-                            "expected <expression> after '(' or ','"
-                        )
-
-                    parameters.append(
-                        FunctionDefinitionArg(
-                            ArgType.NORMAL,
-                            token.text,
-                            name_token=token,
-                        )
-                    )
-
-            parser.push_state()
-            comma = parser.lexer.parse_token()
-            if comma.token_type == TokenType.COMMA:
-                parser.pop_state()
-                continue
-            elif comma.token_type != TokenType.CLOSING_ROUND_BRACKET:
-                parser.rollback_state()
-                parser.lexer.raise_positioned_syntax_error(
-                    "expected ',' or ')' after <parameter>"
-                )
-
-            parser.pop_state()
-            break
+        parameters = cls.parse_parameters(parser)
 
         parser.push_state()
         colon = parser.lexer.parse_token()
@@ -371,6 +205,188 @@ class FunctionDefinitionNode(AbstractSyntaxTreeNode):
             generic_pair,
             generics,
         )
+
+    @classmethod
+    def parse_parameters(cls, parser):
+        parameters = []
+        while True:
+            parser.push_state()
+            token = parser.lexer.parse_token()
+
+            if token.token_type == TokenType.CLOSING_ROUND_BRACKET:
+                parser.pop_state()
+                break
+
+            if token.token_type == TokenType.STAR:
+                cls.parse_parameter_star(parameters, parser, token)
+
+            else:
+                cls.parse_parameter_no_star(parameters, parser, token)
+
+            parser.push_state()
+            comma = parser.lexer.parse_token()
+            if comma.token_type == TokenType.COMMA:
+                parser.pop_state()
+                continue
+            elif comma.token_type != TokenType.CLOSING_ROUND_BRACKET:
+                parser.rollback_state()
+                parser.lexer.raise_positioned_syntax_error(
+                    "expected ',' or ')' after <parameter>"
+                )
+
+            parser.pop_state()
+            break
+        return parameters
+
+    @classmethod
+    def parse_parameter_no_star(cls, parameters, parser, token):
+        is_keyword = False
+        if token.token_type == TokenType.IDENTIFIER:
+            next_token = parser.lexer.parse_token()
+
+            if next_token.token_type == TokenType.EQUAL_SIGN:
+                is_keyword = True
+
+                expr = parser.try_parse_expression()
+
+                if expr is None:
+                    parser.lexer.raise_positioned_syntax_error(
+                        "expected <expression> after '='"
+                    )
+
+                parser.pop_state()
+                parameters.append(
+                    FunctionDefinitionArg(
+                        ArgType.KEYWORD,
+                        token.text,
+                        expr,
+                        name_token=token,
+                        equal_sign_token=next_token,
+                    )
+                )
+            else:
+                parser.rollback_state()
+
+        else:
+            parser.rollback_state()
+        if not is_keyword:
+            expr = parser.try_parse_expression()
+
+            if expr is None:
+                parser.lexer.raise_positioned_syntax_error(
+                    "expected <expression> after '(' or ','"
+                )
+
+            parameters.append(
+                FunctionDefinitionArg(
+                    ArgType.NORMAL,
+                    token.text,
+                    name_token=token,
+                )
+            )
+
+    @classmethod
+    def parse_parameter_star(cls, parameters, parser, token):
+        parser.push_state()
+        next_token = parser.lexer.parse_token()
+        if next_token.token_type == TokenType.STAR:
+            parser.pop_state()
+            parser.push_state()
+            param_name = parser.lexer.parse_token()
+
+            if param_name.token_type != TokenType.IDENTIFIER:
+                parser.rollback_state()
+                parser.lexer.raise_positioned_syntax_error(
+                    "expected <expression> or '*' after '*'"
+                )
+
+            parser.pop_state()
+            parser.pop_state()
+
+            parameters.append(
+                FunctionDefinitionArg(
+                    ArgType.STAR_STAR,
+                    param_name.text,
+                    name_token=token,
+                )
+            )
+
+        else:
+            parser.rollback_state()
+            parser.push_state()
+            param_name = parser.lexer.parse_token()
+
+            if param_name.token_type != TokenType.IDENTIFIER:
+                parser.rollback_state()
+                parser.lexer.raise_positioned_syntax_error(
+                    "expected <expression> or '*' after '*'"
+                )
+
+            parser.pop_state()
+            parser.pop_state()
+
+            parameters.append(
+                FunctionDefinitionArg(
+                    ArgType.STAR,
+                    param_name.text,
+                    name_token=token,
+                )
+            )
+
+    @classmethod
+    def parse_generics(cls, generic_commas, generic_pair, generics, opening, parser):
+        parser.push_state()
+        generic_name = parser.lexer.parse_token()
+        if generic_name.token_type == TokenType.CLOSING_SQUARE_BRACKET:
+            generic_pair = opening, generic_name
+            opening = None
+            parser.pop_state()
+
+        elif generic_name.token_type != TokenType.IDENTIFIER:
+            parser.rollback_state()
+            parser.lexer.raise_positioned_syntax_error(
+                "expected <name> after '[' in <function definition> - <generics>"
+            )
+
+        else:
+            parser.pop_state()
+            generics.append(generic_name)
+
+            while True:
+                parser.push_state()
+                comma = parser.lexer.parse_token()
+
+                if comma.token_type == TokenType.CLOSING_SQUARE_BRACKET:
+                    generic_pair = opening, comma
+                    opening = None
+                    parser.pop_state()
+                    parser.pop_state()
+                    break
+
+                elif comma.token_type != TokenType.COMMA:
+                    parser.rollback_state()
+                    parser.lexer.raise_positioned_syntax_error(
+                        "expected ',' or ']' after <name> in <function definition> - <generics>"
+                    )
+
+                generic_commas.append(comma)
+
+                generic_name = parser.lexer.parse_token()
+                if generic_name.token_type == TokenType.CLOSING_SQUARE_BRACKET:
+                    generic_pair = opening, generic_name
+                    opening = None
+                    parser.pop_state()
+                    parser.pop_state()
+                    break
+
+                elif generic_name.token_type != TokenType.IDENTIFIER:
+                    parser.lexer.raise_positioned_syntax_error(
+                        "expected <name> or ']' after ','  in <function definition> - <generics>"
+                    )
+
+                parser.pop_state()
+                generics.append(generic_name)
+        return generic_pair, opening
 
     def __init__(
         self,
