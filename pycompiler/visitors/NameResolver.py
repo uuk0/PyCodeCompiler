1861220826bd75f423ee715a3a81a6fc013e0070
uuk0 <baulukas1301@googlemail.com>
2023-10-27 17:07:34 +0200

    started defining compiler options

--- pycompiler/visitors/NameResolver.py
+++ pycompiler/visitors/NameResolver.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from pycompiler.parser.NameAccessNode import NameAccessNode
+from pycompiler.parser.NameAccessNode import NameAccessNode, NameWriteAccessNode
 from pycompiler.visitors.AbstractSyntaxTreeVisitor import AbstractASTTreeVisitor
+from pycompiler import options
 
 
 class NameResolver(AbstractASTTreeVisitor):
@@ -10,3 +11,10 @@ class NameResolver(AbstractASTTreeVisitor):
             access.scope.get_access_node_to_name(access.name).copy()
         ):
             raise RuntimeError(f"failed to replace parent of {access}: {access.parent}")
+
+    def visit_write_name_access(self, access: NameWriteAccessNode):
+        if (
+            not options.ENABLE_BUILTIN_SHADOWING
+            and access.name in access.scope.BASE_SCOPE_VALUES
+        ):
+            raise NameError(f"Name {access.name} shadows outer builtin!")
